12c970ed0937c24ede4df327090ea089c6bddce3
Brienyll <Brienyll29@gmail.com>
2019-07-26 00:35:39 -0700

    added layout colors

---
+++ src/components/layout.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import "./layout.css"
+
+const layout = ({children}) => {
+  return (
+    <>
+      <main>
+        {children}
+      </main>
+
+    </>
+  )
+}
+
+export default layout
